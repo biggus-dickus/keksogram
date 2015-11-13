1ad17aa679f9967059977308535d07007371a94b
Max <lynchnost@gmail.com>
2015-11-13 17:20:18 +0200

    Написал функцию getMessage

---
+++ js/check.js
@@ -0,0 +1,35 @@
+function getMessage(a, b) {
+	if (typeof a === "boolean") {
+		if (a) {
+			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+		} else {
+			return "Переданное GIF-изображение не анимировано";
+		}
+	}
+
+	else if (typeof a === "number") {
+		return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+	}
+
+	else if ((typeof a === "object") && (typeof b === "object")) {
+		if ((Array.isArray(a) && Array.isArray(b)) && (a.length == b.length)) {
+			for (var square = 0, i = 0; i < a.length; i++) {
+				square += (a[i] * b[i]);
+			}
+			return "Общая площадь артефактов сжатия: " + square + " пикселей";
+		}
+	}
+
+	else if (typeof a === "object") {
+		if (Array.isArray(a)) {
+			for (var sum = 0, i = 0; i < a.length; i++) {
+				sum += a[i];
+			}
+			return "Количество красных точек во всех строчках изображения: " + sum;
+		}
+	}
+
+	else {
+		return "Что-то пошло не так...";
+	}
+}

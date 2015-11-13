1264933707707a7c95caddd9ac0bae56bc619951
biggus-dickus <lynchnost@gmail.com>
2015-11-13 22:03:35 +0200

    Правки к стилю и форматированию кода

--- js/check.js
+++ js/check.js
@@ -1,35 +1,28 @@
 function getMessage(a, b) {
-	if (typeof a === "boolean") {
+	if (typeof a == 'boolean') {
 		if (a) {
-			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 		} else {
-			return "Переданное GIF-изображение не анимировано";
+			return 'Переданное GIF-изображение не анимировано';
 		}
-	}
-
-	else if (typeof a === "number") {
-		return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
-	}
-
-	else if ((typeof a === "object") && (typeof b === "object")) {
-		if ((Array.isArray(a) && Array.isArray(b)) && (a.length == b.length)) {
-			for (var square = 0, i = 0; i < a.length; i++) {
-				square += (a[i] * b[i]);
+	} else if (typeof a == 'number') {
+			return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов';
+		} else if ((typeof a == 'object') && (typeof b == 'object')) {
+			//Сначала проверяем, что оба аргумента являются массивами и выполняем этот блок кода раньше
+				if ((Array.isArray(a) && Array.isArray(b)) && (a.length == b.length)) {
+					for (var square = 0, i = 0; i < a.length; i++) {
+						square += (a[i] * b[i]);
+					}
+				return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
 			}
-			return "Общая площадь артефактов сжатия: " + square + " пикселей";
-		}
-	}
-
-	else if (typeof a === "object") {
+		} else if (typeof a == 'object') {
 		if (Array.isArray(a)) {
 			for (var sum = 0, i = 0; i < a.length; i++) {
 				sum += a[i];
 			}
-			return "Количество красных точек во всех строчках изображения: " + sum;
+			return 'Количество красных точек во всех строчках изображения: ' + sum;
 		}
-	}
-
-	else {
-		return "Что-то пошло не так...";
+	} else {
+		return 'Что-то пошло не так...';
 	}
 }

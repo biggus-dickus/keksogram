d55a92c4e4bdb35705edaaa5af6514070a1f68dd
biggus-dickus <lynchnost@gmail.com>
2015-11-22 20:23:13 +0200

    Трэвису не нравится alert

--- js/upload.js
+++ js/upload.js
@@ -112,7 +112,7 @@
     if ((+resizeX.value + +resizeSize.value) > currentResizer._image.naturalWidth || (+resizeY.value + +resizeSize.value) > currentResizer._image.naturalHeight) {
       resizeFwd.setAttribute('disabled', true);
       resizeFwd.classList.add('upload-form-controls-fwd--disabled');
-      alert('Введенные значения должны быть заданы в пределах ширины и высоты загруженного изображения.');
+      confirm('Введенные значения должны быть заданы в пределах ширины и высоты загруженного изображения.');
       return false;
     } else {
       resizeFwd.removeAttribute('disabled');

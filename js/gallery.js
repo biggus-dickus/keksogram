98c4a9e3ffeb447bb0b6bc9f255bd56da5e93888
biggus-dickus <lynchnost@gmail.com>
2015-12-14 00:26:23 +0200

    module7-task2 done

---
+++ js/gallery.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function() {
+  function Gallery() {
+    this.element = document.querySelector('.gallery-overlay');
+    this._closeButton = document.querySelector('.gallery-overlay-close');
+    this._image = document.querySelector('.gallery-overlay-image');
+
+    // Привязка контекста обработки событий мыши и клавиатуры
+    // к объекту Gallery прямо в конструкторе.
+    this._onCloseClick = this._onCloseClick.bind(this);
+    this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+  }
+
+  // Показ галереи
+  Gallery.prototype.show = function() {
+    this.element.classList.remove('invisible');
+
+    // Отслеживание событий мыши и клавиатуры в отображенной галерее.
+    this._closeButton.addEventListener('click', this._onCloseClick);
+    this._image.addEventListener('click', this._onPhotoClick);
+    document.addEventListener('keydown', this._onDocumentKeyDown);
+  };
+
+  // Скрытие галереи и удаление обработчиков ее событий.
+  Gallery.prototype.hide = function() {
+    this.element.classList.add('invisible');
+    this._closeButton.removeEventListener('click', this._onCloseClick);
+    this._image.removeEventListener('click', this._onPhotoClick);
+    document.removeEventListener('keydown', this._onDocumentKeyDown);
+  };
+
+  /**
+  * Обработчики клика по крестику, нажатия на ESC и клика по фотографии.
+  * @private
+  */
+  Gallery.prototype._onCloseClick = function() {
+    this.hide();
+  };
+
+  Gallery.prototype._onDocumentKeyDown = function(evt) {
+    if (evt.keyCode === 27) {
+      this.hide();
+    }
+  };
+
+  Gallery.prototype._onPhotoClick = function() {
+    console.log('Обработчик клика по фотографии работает.');
+  };
+
+  window.Gallery = Gallery;
+})();
